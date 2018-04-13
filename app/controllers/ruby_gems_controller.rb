5e7f610b943175fc20b80d5d7f58dba074fecaf7
James Rucker <james@rucker.land>
2018-04-13 11:14:52 -0700

    Add the worst search algorithm as a placeholder.

--- app/controllers/ruby_gems_controller.rb
+++ app/controllers/ruby_gems_controller.rb
@@ -1,5 +1,6 @@
 class RubyGemsController < ApplicationController
   def index
+    @rubygems = search_results
   end
 
   def show
@@ -7,6 +8,18 @@ class RubyGemsController < ApplicationController
   end
 
   private
+
+  def search_results
+    if search_params[:q].present?
+      # Worst Search Algorithm
+      random_offset = [0, rand(RubyGem.count) - 10].max
+      random_size = rand(100)
+
+      RubyGem.offset(random_offset).limit(random_size)
+    else
+      []
+    end
+  end
 
   def search_params
     params.permit(:q)
