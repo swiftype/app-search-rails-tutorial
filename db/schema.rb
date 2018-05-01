ebce944517902f3aad77b8c4afcdf7762154143e
James Rucker <james.rucker@elastic.co>
2018-05-01 16:56:54 -0700

    Switch from Postgres to SQLite, to ease setup. Add data file.

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2018_04_13_033013) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "ruby_gems", force: :cascade do |t|
     t.string "name"
     t.string "authors"
