95036f7f6a2b3c3fa9ffde85ba93d77b4013de5f
Kellen <root@goodroot.ca>
2018-07-13 11:47:36 -0700

    Revert "Completed"

--- app/controllers/ruby_gems_controller.rb
+++ app/controllers/ruby_gems_controller.rb
@@ -5,31 +5,11 @@ class RubyGemsController < ApplicationController
   def index
     if search_params[:q].present?
       @current_page = (search_params[:page] || 1).to_i
+      @total_pages = (RubyGem.count / PAGE_SIZE.to_f).ceil
 
-      search_client = Search.client
-      search_options = {
-        search_fields: {
-          name: { weight: 2.0 },
-          info: {},
-          authors: {},
-        },
-        page: {
-          current: @current_page,
-          size: PAGE_SIZE,
-        },
-      }
+      page_offset = (@current_page - 1) * PAGE_SIZE
 
-      if search_params[:popular].present?
-        search_options[:filters] = {
-          downloads: { from: 1_000_000 },
-        }
-      end
-
-      search_response = search_client.search(Search::ENGINE_NAME, search_params[:q], search_options)
-      @total_pages = search_response['meta']['page']['total_pages']
-      result_ids = search_response['results'].map { |rg| rg['id']['raw'].to_i }
-
-      @search_results = RubyGem.where(id: result_ids).sort_by { |rg| result_ids.index(rg.id) }
+      @search_results = RubyGem.order(:created_at).limit(PAGE_SIZE).offset(page_offset)
     end
   end
 
@@ -40,6 +20,6 @@ class RubyGemsController < ApplicationController
   private
 
   def search_params
-    params.permit(:q, :page, :popular)
+    params.permit(:q, :page)
   end
 end
